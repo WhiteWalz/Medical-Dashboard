87e47335cf7a0f8696dbf491e0e65740f341e5c3
J. White <whijus602@gmail.com>
2023-11-14 23:01:41 -0500

    Adds User model and login manager

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, Blueprint
+from flask_login import LoginManager
 from app.auth import bp as auth_bp
 from app.general import bp as general_bp
 
 app = Flask(__name__)
+loginmanager = LoginManager()
+loginmanager.init_app(app)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(general_bp)
---
+++ app/models/User.py
@@ -0,0 +1,51 @@
+from app import db, loginmanager
+from flask_login import UserMixin
+from hashlib import sha256
+from dotenv import load_dotenv
+from enum import Enum
+
+load_dotenv('../.env')
+
+class roles(Enum):
+    ADMIN = 0
+    MANAGER = 1
+    INVENTORY = 2
+    RECEPTION = 3
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    role = db.Column(db.Integer, nullable=False)
+    is_authenticated = True
+    is_active = db.Column(db.String(5), nullable=False)
+    is_anonymous = False
+
+    def __init__(self, email: str, pwd: str, role: int):
+        self.email = email
+        self.password = sha256(SALT + pwd)
+        self.role = role
+
+    def get_id(self):
+        return str(self.id)
+
+    def findByID(id):
+        return User.query.filter_by(user_id=id).first()
+    
+    def findByEmail(email: str):
+        return User.query.filter_by(email=email).first()
+    
+    def verifyLogin(email: str, password: str):
+        user = User.findByEmail(email)
+        if user is None:
+            return None
+        pwdHash = sha256(SALT + password)
+
+        if user.password == pwdHash:
+            return user.id
+        
+        return None
+    
+@loginmanager.user_loader
+def loadUser(id):
+    return User.findByID(id)
