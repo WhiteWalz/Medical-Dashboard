dff864be2b00d856243f3108e4b0acd61c5c3e75
J. White <whijus602@gmail.com>
2023-11-30 23:52:44 -0500

    Adjusts helper classes to shift to prioritizing hour to hour consistency

--- utilities/scheduler.py
+++ utilities/scheduler.py
@@ -15,15 +15,23 @@ class DoctorDay:
 class Employee:
     #   daysWorked should be a dictionary of dictionaries where the keys are the days worked, and the values are dicts where the keys are
     #       keywords like start, end, max representing the start time, end time, and max hours for a given day, in integer form.
-    #   doctorWeights is a dictionary where the keys are doctorIDs and the values are weights based on how often the person has worked that doctor
-    #       or manually set.
 
-    def __init__(self, id: int, daysWorked: dict, doctorWeights: dict):
+    def __init__(self, id: int, daysWorked: dict):
         self.id = id
         self.daysWorked = daysWorked
-        self.doctorWeights = doctorWeights
+        self.hoursWorked = 0
 
     def __init__(self, id: int):
         self.id = id
         self.daysWorked = dict()
-        self.doctorWeights = dict()
+        self.hoursWorked = 0
+
+class Node:
+
+    def __init__(self, children: list, score: float, parentContext: dict, doctors: list, employees: list):
+        self.children = Node.expand(self)
+        self.score = score
+        self.possScore = Node.calcBestScore(self)
+        self.parentContext = parentContext
+        self.doctors = doctors
+        self.employees = employees
